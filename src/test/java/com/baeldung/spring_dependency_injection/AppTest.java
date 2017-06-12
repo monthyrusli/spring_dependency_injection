82bf2d72432e424d5cd71f9a0d6efc7f84165665
monthyrusli <monthy.rusli@gmail.com>
2017-06-13 06:13:49 +0700

    Fixing JUnit Test

--- src/test/java/com/baeldung/spring_dependency_injection/AppTest.java
+++ src/test/java/com/baeldung/spring_dependency_injection/AppTest.java
@@ -1,55 +1,48 @@
 package com.baeldung.spring_dependency_injection;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
+    @Test
+    public void givenJavaConfiguration_WhenOnConstructor_ThenValidDependency() {
         ApplicationContext contextJava = new AnnotationConfigApplicationContext(Config.class);
 
         Person personJavaConstructor = (Person) contextJava.getBean("personJavaConstructor");
         assertEquals("James", personJavaConstructor.getName());
         assertEquals(24, personJavaConstructor.getAge());
+    }
+
+    @Test
+    public void givenJavaConfiguration_WhenOnSetter_ThenValidDependency() {
+        ApplicationContext contextJava = new AnnotationConfigApplicationContext(Config.class);
 
         Person personJavaSetter = (Person) contextJava.getBean("personJavaSetter");
         assertEquals("John", personJavaSetter.getName());
         assertEquals(25, personJavaSetter.getAge());
+    }
 
+    @Test
+    public void givenXmlConfiguration_WhenOnConstructor_ThenValidDependency() {
         ApplicationContext contextXml = new ClassPathXmlApplicationContext("spring-config.xml");
 
         Person personXmlConstructor = (Person) contextXml.getBean("personXmlConstructor");
         assertEquals("Robert", personXmlConstructor.getName());
         assertEquals(26, personXmlConstructor.getAge());
+    }
+
+    @Test
+    public void givenXmlConfiguration_WhenOnSetter_ThenValidDependency() {
+        ApplicationContext contextXml = new ClassPathXmlApplicationContext("spring-config.xml");
 
         Person personXmlSetter = (Person) contextXml.getBean("personXmlSetter");
         assertEquals("Michael", personXmlSetter.getName());
         assertEquals(27, personXmlSetter.getAge());
     }
+
 }
